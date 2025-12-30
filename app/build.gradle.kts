994e178abc7747745bbe442b0158157422b410f6
ENT <abolhasani6110@gmail.com>
2025-12-30 21:54:05 +0330

    Setup initial Android project structure for personal expense app

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+    id("androidx.navigation.safeargs.kotlin") version "2.9.6"
+    kotlin("plugin.serialization") version "2.0.21"
+
+}
+android {
+    namespace = "com.ent.cashmate"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ent.cashmate"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(files("lib/android.jar"))
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    kotlin("plugin.serialization")
+    implementation("uk.co.chrisjenx:calligraphy:2.3.0")
+
+
+
+
+}
---
+++ app/src/main/java/com/ent/cashmate/Ui/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ent.cashmate.Ui.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ent.cashmate.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
